e9c8e0024e41ba8dcf0bf152e1d7c3e1fbfc06e9
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2022-02-19 09:45:11 +0530

    all products are listed in to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
 import { Grid } from '@mui/material'
-import Head from 'next/head'
-import Image from 'next/image'
 import ProductItem from '../components/ProductItem'
 import data from '../utils/data'
 
@@ -9,11 +7,11 @@ export default function Home() {
     <div>
       <h1>Products</h1>
       <Grid container spacing={3}>
-        {topRatedProducts.map((product) => (
+        {data.products.map((product) => (
           <Grid item md={4} key={product.name}>
             <ProductItem
               product={product}
-              addToCartHandler={addToCartHandler}
+              // addToCartHandler={addToCartHandler}
             />
           </Grid>
         ))}
