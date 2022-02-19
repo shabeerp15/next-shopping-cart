3b7f94a407750846b1639f04a068189f0b387dc2
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2022-02-19 09:08:04 +0530

    initial commit. starting a simple shopping cart application with next js

---
+++ components/layout.js
@@ -0,0 +1,34 @@
+import { AppBar, Container, Toolbar, Typography } from '@mui/material'
+import Head from 'next/head'
+import useStyles from '../utils/styles'
+
+const layout = ({ children }) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const classes = useStyles()
+   return (
+      <div> 
+         <Head>
+            <title>Next Amazon</title>
+            <meta
+               name='viewport'
+               content='initial-scale=1.0, width=device-width'
+            />
+         </Head>
+         <AppBar position='static' className={classes.navbar}>
+            <Toolbar>
+               <Typography variant='title' color='inherit'>
+                  Amazon
+               </Typography>
+            </Toolbar>
+         </AppBar>
+         <Container className={classes.main}>{children}</Container>
+         <footer className={classes.footer}>
+            <Typography>
+               All rights reserved. &copy; {new Date().getFullYear()}
+            </Typography>
+         </footer>
+      </div>
+   )
+}
+
+export default layout
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import Layout from '../components/Layout'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   return (
+      <>
+         <Layout>
+            <Component {...pageProps} />
+         </Layout>
+      </>
+   )
 }
 
 export default MyApp
