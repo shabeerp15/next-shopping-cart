9ffa50f94045ba56fa0ab6858d3ed8fff926a004
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2022-02-24 19:03:21 +0530

    generate token

--- pages/login.js
+++ pages/login.js
@@ -7,13 +7,27 @@ import {
    Link,
 } from '@mui/material'
 import { Controller, useForm } from 'react-hook-form'
-import React from 'react'
+import React, { useState } from 'react'
 import Meta from '../components/Meta'
 import NextLink from 'next/link'
 import useStyles from '../utils/styles'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const LoginScreen = () => {
+   const [email, setEmail] = useState('')
+   const [password, setPassword] = useState('')
    const classes = useStyles()
+
+   const submitHandler = async (e) => {
+      e.preventDefault()
+      try {
+         const { data } = await axios.post('/api/users/login', {email, password})
+         toast.success('Login successful')
+      } catch (error) {
+         toast.error(error.response.data.message || error.message)
+      }
+   }
 
    return (
       <>
@@ -22,7 +36,7 @@ const LoginScreen = () => {
             description={'Login page'}
             keywords={'login, amazon, shopping'}
          />
-         <form className={classes.form}>
+         <form className={classes.form} onSubmit={submitHandler}>
             <Typography component='h1' variant='h1'>
                Login
             </Typography>
@@ -34,9 +48,8 @@ const LoginScreen = () => {
                      id='email'
                      label='Email'
                      inputProps={{ type: 'email' }}
-                  >
-                     {' '}
-                  </TextField>
+                     onChange={e => setEmail(e.target.value)}
+                  ></TextField>
 
                   {/* <Controller
                      name='email'
@@ -74,6 +87,7 @@ const LoginScreen = () => {
                      id='password'
                      label='Password'
                      inputProps={{ type: 'password' }}
+                     onChange={e => setPassword(e.target.value)}
                   ></TextField>
 
                   {/* <Controller
