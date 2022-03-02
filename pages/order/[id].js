7d26c5e072b4d43c21eb25ef85207e1bb5a05b71
LAPTOP-5PNJLMRU\asus <muhammed.s@deepnetsoft.com>
2022-03-02 20:19:32 +0530

    admin product list page

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -9,6 +9,7 @@ import useStyles from '../../utils/styles'
 import { toast } from 'react-toastify'
 import Meta from '../../components/Meta'
 import {
+   Button,
    Card,
    CircularProgress,
    Grid,
@@ -43,7 +44,23 @@ function reducer(state, action) {
          return { ...state, loadingPay: false, errorPay: action.payload }
       case 'PAY_RESET':
          return { ...state, loadingPay: false, successPay: false, errorPay: '' }
-
+      case 'DELIVER_REQUEST':
+         return { ...state, loadingDeliver: true }
+      case 'DELIVER_SUCCESS':
+         return { ...state, loadingDeliver: false, successDeliver: true }
+      case 'DELIVER_FAIL':
+         return {
+            ...state,
+            loadingDeliver: false,
+            errorDeliver: action.payload,
+         }
+      case 'DELIVER_RESET':
+         return {
+            ...state,
+            loadingDeliver: false,
+            successDeliver: false,
+            errorDeliver: '',
+         }
       default:
          return state
    }
@@ -57,14 +74,14 @@ const Order = ({ params }) => {
    const { state } = useContext(Store)
    const { userInfo } = state
 
-   const [{ loading, error, order, successPay }, dispatch] = useReducer(
-      reducer,
-      {
-         loading: true,
-         order: {},
-         error: '',
-      }
-   )
+   const [
+      { loading, error, order, successPay, loadingDeliver, successDeliver },
+      dispatch,
+   ] = useReducer(reducer, {
+      loading: true,
+      order: {},
+      error: '',
+   })
 
    const {
       shippingAddress,
@@ -96,10 +113,13 @@ const Order = ({ params }) => {
             // toast.error(getError(err))
          }
       }
-      if (!order._id || successPay || order._id !== orderId) {
+      if (!order._id || successPay || successDeliver || order._id !== orderId) {
          fetchOrder()
          if (successPay) {
             dispatch({ type: 'PAY_RESET' })
+         }
+         if (successDeliver) {
+            dispatch({ type: 'DELIVER_RESET' })
          }
       } else {
          const loadPaypalScript = async () => {
@@ -117,7 +137,25 @@ const Order = ({ params }) => {
          }
          loadPaypalScript()
       }
-   }, [order, successPay])
+   }, [order, successPay, successDeliver])
+
+   async function deliverOrderHandler() {
+      try {
+         dispatch({ type: 'DELIVER_REQUEST' })
+         const { data } = await axios.put(
+            `/api/orders/${order._id}/deliver`,
+            {},
+            {
+               headers: { authorization: `Bearer ${userInfo.token}` },
+            }
+         )
+         dispatch({ type: 'DELIVER_SUCCESS', payload: data })
+         enqueueSnackbar('Order is delivered', { variant: 'success' })
+      } catch (err) {
+         dispatch({ type: 'DELIVER_FAIL', payload: getError(err) })
+         enqueueSnackbar(getError(err), { variant: 'error' })
+      }
+   }
 
    function createOrder(data, actions) {
       return actions.order
@@ -192,7 +230,9 @@ const Order = ({ params }) => {
                            <ListItem>
                               Status:{' '}
                               {isDelivered
-                                 ? `Delivered on ${moment(deliveredAt).fromNow()}`
+                                 ? `Delivered on ${moment(
+                                      deliveredAt
+                                   ).fromNow()}`
                                  : 'Pending'}
                            </ListItem>
                         </List>
@@ -358,6 +398,21 @@ const Order = ({ params }) => {
                                  )}
                               </ListItem>
                            )}
+                           {userInfo.isAdmin &&
+                              order.isPaid &&
+                              !order.isDelivered && (
+                                 <ListItem>
+                                    {loadingDeliver && <CircularProgress />}
+                                    <Button
+                                       fullWidth
+                                       variant='contained'
+                                       color='primary'
+                                       onClick={deliverOrderHandler}
+                                    >
+                                       Deliver Order
+                                    </Button>
+                                 </ListItem>
+                              )}
                         </List>
                      </Card>
                   </Grid>
